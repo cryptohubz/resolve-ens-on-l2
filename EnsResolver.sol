376a07b4343c460698521c8a4879c145f42da298
cryptohubz <78040885+cryptohubz@users.noreply.github.com>
2024-07-14 02:52:45 +0200

    Update EnsResolver.sol

--- EnsResolver.sol
+++ EnsResolver.sol
@@ -19,81 +19,6 @@ contract ResolveENS {
     address constant L1_SLOAD_ADDRESS = 0x0000000000000000000000000000000000000101;
     uint256 constant DEFAULT_COIN_TYPE = 60;
 
-
-    function readSingleSlot(address l1_contract, uint256 slot) public view returns (uint256) {
-
-        bytes memory input = abi.encodePacked(l1_contract, slot);
-
-        bool success;
-        bytes memory result;
-
-        (success, result) = L1_SLOAD_ADDRESS.staticcall(input);
-
-        if (!success) {
-            revert("L1SLOAD failed");
-        }
-
-        return abi.decode(result, (uint256));
-
-    }
-
-    function readMultiSlot(address l1_contract, uint256[] memory slots) public view returns (bytes32[] memory) {
-
-        bytes memory input = abi.encodePacked(l1_contract, slots);
-
-        bool success;
-        bytes memory result;
-
-        (success, result) = L1_SLOAD_ADDRESS.staticcall(input);
-
-        if (!success) {
-            revert("L1SLOAD failed");
-        }
-
-        return convertBytesToBytes32Array(result);
-
-    }
-
-    // readr recordVersion
-    // function readRecordVersion(address l1_contract, bytes32 node, uint256 slot) public view returns (bytes memory) {
-    //     // Calculate the slot for recordVersion
-    //     bytes32 recordVersionSlotHash = keccak256(abi.encode(node, slot));
-    //     // Convert bytes32 to dynamic bytes array
-    //     // bytes memory hashed = new bytes(32);
-    //     // for (uint8 i = 0; i < 32; i++) {
-    //     //     hashed[i] = recordVersionSlotHash[i];
-    //     // }
-    //     bytes32 slotHash = keccak256(abi.encode(l1_contract, recordVersionSlotHash));
-    //     // bytes memory hashedSlot = new bytes(32);
-    //     // for (uint8 i = 0; i < 32; i++) {
-    //     //     hashedSlot[i] = slotHash[i];
-    //     // }
-    //     bool success;
-    //     bytes memory result;
-    //     (success, result) = L1_SLOAD_ADDRESS.staticcall(slotHash);
-        
-    //     // if (!success) {
-    //     //     revert("L1SLOAD failed to read recore version");
-    //     // }
-
-    //     return result;
-    // }
-
- // readr2 recordVersion
-    function readUintRecordVersion(address l1_contract, bytes32 node, uint256 slot) public view returns (uint) {
-        // Calculate the slot for recordVersion
-        bytes memory recordVersionHash = abi.encodePacked(l1_contract, slot, node);
-        bool success;
-        bytes memory result;
-        (success, result) = L1_SLOAD_ADDRESS.staticcall(recordVersionHash);
-        
-        if (!success) {
-            revert("L1SLOAD failed to read recore version");
-        }
-
-        return abi.decode(result, (uint256));
-    }
-
     function retrieveSlotFromL1(address l1StorageAddress, uint slot) internal view returns (bytes memory) {
         bool success;
         bytes memory returnValue;
@@ -105,67 +30,41 @@ contract ResolveENS {
         return returnValue;
     }
 
-
       function resolveEnsName(address l1_contract, bytes32 node, uint256 slot, uint256 recordVersion, uint256 coinType) public view returns (bytes memory) {
-        // Calculate the slot for recordVersion
-        // versionable_addresses[0]["myname.eth"][60];
-        uint slotNumber1 = uint(keccak256(abi.encodePacked(slot,node)));
-        uint slotNumber2 = uint(keccak256(abi.encodePacked(uint(uint160(recordVersion)),slotNumber1) ));
-        uint slotNumber3 = uint(keccak256(abi.encodePacked(uint(uint160(coinType)),slotNumber2) ));
-        return abi.decode(retrieveSlotFromL1(
-            l1_contract,
-            slotNumber3
-            ), (bytes));
-    }
-
-      function resolveEns(address l1_contract, bytes32 node, uint256 slot, uint256 recordVersion, uint256 coinType) public view returns (bytes memory) {
-        // Calculate the slot for recordVersion
-        // versionable_addresses[0]["myname.eth"][60];
+        //Calculate the slot for the top-level mapping (version)
         bytes32 topSlot = keccak256(abi.encodePacked(uint256(recordVersion), uint256(slot)));
-        bytes32 middleSlot = keccak256(abi.encodePacked(node, topSlot));
-        uint256 finalSlot = uint256(keccak256(abi.encodePacked(coinType, middleSlot)));
 
+        //Calculate the slot for the second-level mapping (node)
+        bytes32 middleSlot = keccak256(abi.encodePacked(node, topSlot));
+
+        //Calculate the slot for the innermost mapping (coinType)
+        uint256 finalSlot = uint256(keccak256(abi.encodePacked(coinType, middleSlot)));
         return abi.decode(retrieveSlotFromL1(
             l1_contract,
             finalSlot
             ), (bytes));
     }
-
-
-    
-
-    function resolveEnsName3(address l1_contract, bytes32 node, uint256 slot, uint256 recordVersion, uint256 coinType) public view returns (bytes memory) {
-        // Calculate the slot for recordVersion
-        // versionable_addresses[0]["myname.eth"][60];
-        uint slotNumber = uint(keccak256(abi.encodePacked(slot, recordVersion, node, coinType)));
-
-        return abi.decode(retrieveSlotFromL1(
-            l1_contract,
-            slotNumber
-            ), (bytes));
-    }
-    
-    function resolveEnsName5(address l1_contract, bytes32 node, uint256 slot, uint256 recordVersion, uint256 coinType) public view returns (bytes memory) {
-        // Calculate the slot for recordVersion
-        // versionable_addresses[0]["myname.eth"][60];
-        uint slotNumber = uint(keccak256(abi.encodePacked( uint(recordVersion), uint(node), uint(coinType), uint(slot))));
-
-        return abi.decode(retrieveSlotFromL1(
-            l1_contract,
-            slotNumber
-            ), (bytes));
+  
+      function toString(uint256 value) internal pure returns (string memory) {
+        // Inspired by OraclizeAPI's implementation - MIT license
+        if (value == 0) {
+            return "0";
+        }
+        uint256 temp = value;
+        uint256 digits;
+        while (temp != 0) {
+            digits++;
+            temp /= 10;
+        }
+        bytes memory buffer = new bytes(digits);
+        while (value != 0) {
+            digits -= 1;
+            buffer[digits] = bytes1(uint8(48 + uint256(value % 10)));
+            value /= 10;
+        }
+        return string(buffer);
     }
 
-    function resolveEnsName4(address l1_contract, bytes32 node, uint256 slot, uint256 recordVersion, uint256 coinType) public view returns (bytes memory) {
-        // Calculate the slot for recordVersion
-        // versionable_addresses[0]["myname.eth"][60];
-        uint slotNumber = uint(keccak256(abi.encodePacked(coinType, node, recordVersion, slot)));
-
-        return abi.decode(retrieveSlotFromL1(
-            l1_contract,
-            slotNumber
-            ), (bytes));
-    }
     function convertBytesToBytes32Array(bytes memory data) private pure returns (bytes32[] memory) {
         // Calculate the number of 32-byte chunks
         uint256 length = data.length;
@@ -186,4 +85,3 @@ contract ResolveENS {
     }
 
 }
-
